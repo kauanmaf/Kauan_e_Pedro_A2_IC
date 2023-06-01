e7032cc7a645fc1b90a3e500a000a87c4ae509ed
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2023-06-01 14:08:15 -0300

    Adicionei função 1

--- a2.py
+++ a2.py
@@ -1,1 +1,11 @@
 AUTORES = ["Kauan Mariani Ferreira", "Pedro Henrique Coterli"]
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+def questao_1(datapath):
+    tabela = pd.read_csv(datapath)
+    tamanho = len(tabela.index)
+    return tamanho
+
