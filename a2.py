6e9a6616318a671f69c5410a470aed5574b7028e
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2023-06-14 15:37:42 -0300

    Arrumei questão 7

--- a2.py
+++ a2.py
@@ -50,12 +50,13 @@ def questao_7(datapath = DATA):
     mun_por_est = {31 : 853, 35 : 645, 43 : 497, 29 : 417, 41 : 399, 42 : 295, 52 : 246, 22 : 224, 25 : 223, 21: 217, 26 : 184, 23 : 184, 24 : 167, 15 : 144, 51 : 141, 17 : 139, 27 : 102, 33 : 92, 50 : 79, 32 : 78, 28 : 75, 13 : 62,11 : 52, 12 : 22, 16 : 16, 14 : 15}
 
     casos_por_estado = tabela.groupby('SG_UF_NOT')['ID_MUNICIP'].nunique()
+    casos_por_estado.index = pd.to_numeric(casos_por_estado.index)
     tabela_auxiliar = pd.DataFrame({"COD": mun_por_est.keys(), "Quantidade de Cidades": mun_por_est.values()})
     tabela_auxiliar["Quantidade de casos"] = tabela_auxiliar["COD"].map(casos_por_estado).fillna(0)
     tabela_auxiliar["Porcentagem"] = (tabela_auxiliar["Quantidade de casos"]*100/tabela_auxiliar["Quantidade de Cidades"]).round(2)
     tabela_auxiliar["UF"] = tabela_auxiliar["COD"].map(codigos_estados)
     dicionario = dict(zip(tabela_auxiliar["UF"], tabela_auxiliar["Porcentagem"]))
-    return tabela_auxiliar
+    return dicionario
 
 def questao_8(datapath = DATA):
     tabela = pd.read_parquet(datapath)
@@ -64,4 +65,3 @@ def questao_8(datapath = DATA):
     tabela["ATRASO_NOT"] = tabela["DT_NOTIFICACAO"] - tabela["DT_SINTOMAS"]
     return tabela
 
-print(questao_5())
