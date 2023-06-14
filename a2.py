918d61b18f682b517fb9d983cdd9b96bbf935135
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-06-14 14:53:12 -0300

    Commit rápido

--- a2.py
+++ a2.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 # import matplotlib as plt
 
+codigos_estados = {35: 'SP', 41: 'PR', 42: 'SC', 43: 'RS', 50: 'MS', 11: 'RO', 12: 'AC', 13: 'AM', 14: 'RR', 15: 'PA', 16: 'AP', 17: 'TO', 21: 'MA', 24: 'RN', 25: 'PB', 26: 'PE', 27: 'AL', 28: 'SE', 29: 'BA', 31: 'MG', 33: 'RJ', 51: 'MT', 52: 'GO', 53: 'DF', 22: 'PI', 23: 'CE', 32: 'ES'}
+
 DATA = "esquistossomose.parquet"
 
 def questao_1(datapath = DATA):
@@ -29,7 +31,6 @@ def questao_4(datapath = DATA):
 def questao_5(datapath = DATA):
     tabela = pd.read_parquet(datapath)
     tabela_por_estado = tabela.value_counts("SG_UF_NOT")
-    codigos_estados = {35: 'SP', 41: 'PR', 42: 'SC', 43: 'RS', 50: 'MS', 11: 'RO', 12: 'AC', 13: 'AM', 14: 'RR', 15: 'PA', 16: 'AP', 17: 'TO', 21: 'MA', 24: 'RN', 25: 'PB', 26: 'PE', 27: 'AL', 28: 'SE', 29: 'BA', 31: 'MG', 33: 'RJ', 51: 'MT', 52: 'GO', 53: 'DF', 22: 'PI', 23: 'CE', 32: 'ES'}
     tabela_por_estado.index = pd.to_numeric(tabela_por_estado.index)
     for codigo, sigla in codigos_estados.items():
         tabela_por_estado.rename(index = {codigo: sigla}, inplace = True)
@@ -39,23 +40,20 @@ def questao_6(datapath = DATA):
     tabela = pd.read_parquet(datapath)
     sexo_masculino = tabela[tabela["CS_SEXO"] == "M"]
     sexo_masculino_por_estado = sexo_masculino.value_counts("SG_UF_NOT")
-    codigos_estados = {35: 'SP', 41: 'PR', 42: 'SC', 43: 'RS', 50: 'MS', 11: 'RO', 12: 'AC', 13: 'AM', 14: 'RR', 15: 'PA', 16: 'AP', 17: 'TO', 21: 'MA', 24: 'RN', 25: 'PB', 26: 'PE', 27: 'AL', 28: 'SE', 29: 'BA', 31: 'MG', 33: 'RJ', 51: 'MT', 52: 'GO', 53: 'DF', 22: 'PI', 23: 'CE', 32: 'ES'}
+    sexo_masculino_por_estado.index = pd.to_numeric(sexo_masculino_por_estado.index)
     for codigo, sigla in codigos_estados.items():
         sexo_masculino_por_estado.rename(index = {codigo: sigla}, inplace = True)
     return sexo_masculino_por_estado.to_dict()
 
-
-
 def questao_7(datapath = DATA):
     tabela = pd.read_parquet(datapath)
     mun_por_est = {31 : 853, 35 : 645, 43 : 497, 29 : 417, 41 : 399, 42 : 295, 52 : 246, 22 : 224, 25 : 223, 21: 217, 26 : 184, 23 : 184, 24 : 167, 15 : 144, 51 : 141, 17 : 139, 27 : 102, 33 : 92, 50 : 79, 32 : 78, 28 : 75, 13 : 62,11 : 52, 12 : 22, 16 : 16, 14 : 15}
-    codigo_estados = {35: 'SP', 41: 'PR', 42: 'SC', 43: 'RS', 50: 'MS', 11: 'RO', 12: 'AC', 13: 'AM', 14: 'RR', 15: 'PA', 16: 'AP', 17: 'TO', 21: 'MA', 24: 'RN', 25: 'PB', 26: 'PE', 27: 'AL', 28: 'SE', 29: 'BA', 31: 'MG', 33: 'RJ', 51: 'MT', 52: 'GO', 53: 'DF', 22: 'PI', 23: 'CE', 32: 'ES'}
 
     casos_por_estado = tabela.groupby('SG_UF_NOT')['ID_MUNICIP'].nunique()
     tabela_auxiliar = pd.DataFrame({"COD": mun_por_est.keys(), "Quantidade de Cidades": mun_por_est.values()})
     tabela_auxiliar["Quantidade de casos"] = tabela_auxiliar["COD"].map(casos_por_estado).fillna(0)
     tabela_auxiliar["Porcentagem"] = (tabela_auxiliar["Quantidade de casos"]*100/tabela_auxiliar["Quantidade de Cidades"]).round(2)
-    tabela_auxiliar["UF"] = tabela_auxiliar["COD"].map(codigo_estados)
+    tabela_auxiliar["UF"] = tabela_auxiliar["COD"].map(codigos_estados)
     dicionario = dict(zip(tabela_auxiliar["UF"], tabela_auxiliar["Porcentagem"]))
     return dicionario 
 
@@ -66,4 +64,4 @@ def questao_8(datapath = DATA):
     tabela["ATRASO_NOT"] = tabela["DT_NOTIFICACAO"] - tabela["DT_SINTOMAS"]
     return tabela
 
-print(questao_5())
+print(questao_3())
